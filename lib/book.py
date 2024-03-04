643d9cfddf828c27ef9cac4deff6cd68c178d296
mayarakok <akokmayar607@gmail.com>
2024-03-04 14:21:34 +0300

    together

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,38 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    
+    def __init__(self,title,page_count):
+        self.title = title
+        self.page_count = page_count
+        # self._turn_page = turn_page
+        
+        
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self,page_count):
+        if type(page_count)== int :
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")    
+     
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+                
+    # @property
+    # def turn_page(self):
+    #     return self._turn_page
+    
+    # @turn_page.setter
+    # def turn_page(self,turn_page):
+    #     self._turn_page = turn_page
+    #     print("Flipping the page...wow, you read fast!")
+      
+        
+    
+        # pass
     
         
